5a27c9fc76d991f36e34cb8546502d591ad5bfe0
mayankpandav <mpandav1998@gmail.com>
2020-09-27 13:24:03 +0530

    npm added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import Package from './Components/package';
 
 class App extends Component {
 
@@ -47,6 +48,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
+        <Package data={this.state.resumeData.npm}/>
         <Testimonials data={this.state.resumeData.testimonials}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
