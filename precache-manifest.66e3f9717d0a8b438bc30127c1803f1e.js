089ada25dd7beb5e0ad854a61317b7f1f31dddf2
mayankpandav <mpandav1998@gmail.com>
2022-01-01 13:06:03 +0530

    his

---
+++ precache-manifest.66e3f9717d0a8b438bc30127c1803f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a9de8ef1164e70dfb845aa5391f782",
+    "url": "/mayankpandav.github.io/index.html"
+  },
+  {
+    "revision": "c498fe4660a91d96e783",
+    "url": "/mayankpandav.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/mayankpandav.github.io/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/mayankpandav.github.io/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c498fe4660a91d96e783",
+    "url": "/mayankpandav.github.io/static/js/main.2f446feb.chunk.js"
+  },
+  {
+    "revision": "0c33d811ed5e54e907c8",
+    "url": "/mayankpandav.github.io/static/js/runtime-main.118a8929.js"
+  }
+]);
